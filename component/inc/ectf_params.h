a2df13530d9cbbf28a87dd165aa815468974a2d4
Purvansh Bhatt <purvansh.bhatt@pace.edu>
2024-04-02 16:06:44 -0400

    final design

--- component/inc/ectf_params.h
+++
@@ -1,8 +0,0 @@
-#ifndef __ECTF_PARAMS__
-#define __ECTF_PARAMS__
-#define COMPONENT_ID 0x99887720
-#define COMPONENT_BOOT_MSG "boot message for Component 1"
-#define ATTESTATION_LOC "New York"
-#define ATTESTATION_DATE "1/25/24"
-#define ATTESTATION_CUSTOMER "University"
-#endif
